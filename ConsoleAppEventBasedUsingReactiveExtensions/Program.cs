ea356704d43165eb2f870498e6c8bce0de0c8c5c
Steven Van Iseghem <steven.van.iseghem@outlook.com>
2019-01-08 19:12:35 +0100

    Adding Events using Rx

--- ConsoleAppEventBasedUsingReactiveExtensions/Program.cs
+++ ConsoleAppEventBasedUsingReactiveExtensions/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Subjects;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,8 +12,12 @@ namespace ConsoleAppEventBasedUsingReactiveExtensions
         // Static action event
         static event Action<string> datatypes;
 
+        // Static subject event using IObservable
+        static Subject<string> obsTypes = new Subject<string>();
+
         static void Main(string[] args)
         {
+
             List<DotNet> lstTypes = new List<DotNet>();
 
             DotNet blnTypes = new DotNet();
@@ -31,19 +36,37 @@ namespace ConsoleAppEventBasedUsingReactiveExtensions
             decTypes.AvailableDatatype = "decimal";
             lstTypes.Add(decTypes);
 
-            //datatypes(lstTypes[i].AvailableDatatype);
+            // datatypes(lstTypes[i].AvailableDatatype);
+            #region += Event Coupling
+            /*
             datatypes += x =>
-            {
-                Console.WriteLine(x);
-            };
+                {
+                    Console.WriteLine(x);
+                };
 
             for (int i = 0; i < lstTypes.Count; i++)
             {
                 datatypes(lstTypes[i].AvailableDatatype);
+            } 
+            */
+            #endregion
+
+            // IObservable
+            obsTypes.Subscribe(x =>
+            {
+                Console.WriteLine(x);
+            });
+
+            // IObserver
+            for (int i = 0; i < lstTypes.Count; i++)
+            {
+                obsTypes.OnNext(lstTypes[i].AvailableDatatype);
             }
 
             Console.WriteLine("Finished event based reactive execution");
             Console.ReadLine();
+
+            // LINQ Language-Integrated Query
 
         }
     }
