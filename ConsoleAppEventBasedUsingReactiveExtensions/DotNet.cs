fefef80f7cfa617943cbdb7c4a74ed707463628e
Steven Van Iseghem <steven.van.iseghem@outlook.com>
2019-01-08 19:02:34 +0100

    Add project files.

---
+++ ConsoleAppEventBasedUsingReactiveExtensions/DotNet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppEventBasedUsingReactiveExtensions
+{
+    class DotNet
+    {
+        public string AvailableDatatype { get; set; }
+    }
+}
---
+++ ConsoleAppEventBasedUsingReactiveExtensions/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppEventBasedUsingReactiveExtensions
+{
+    class Program
+    {
+        // Static action event
+        static event Action<string> datatypes;
+
+        static void Main(string[] args)
+        {
+            List<DotNet> lstTypes = new List<DotNet>();
+
+            DotNet blnTypes = new DotNet();
+            blnTypes.AvailableDatatype = "bool";
+            lstTypes.Add(blnTypes);
+
+            DotNet strTypes = new DotNet();
+            strTypes.AvailableDatatype = "string";
+            lstTypes.Add(strTypes);
+
+            DotNet intTypes = new DotNet();
+            intTypes.AvailableDatatype = "int";
+            lstTypes.Add(intTypes);
+
+            DotNet decTypes = new DotNet();
+            decTypes.AvailableDatatype = "decimal";
+            lstTypes.Add(decTypes);
+
+            //datatypes(lstTypes[i].AvailableDatatype);
+            datatypes += x =>
+            {
+                Console.WriteLine(x);
+            };
+
+            for (int i = 0; i < lstTypes.Count; i++)
+            {
+                datatypes(lstTypes[i].AvailableDatatype);
+            }
+
+            Console.WriteLine("Finished event based reactive execution");
+            Console.ReadLine();
+
+        }
+    }
+}
